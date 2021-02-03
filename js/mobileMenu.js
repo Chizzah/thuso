79a60155467eded5640ca9065b70a4bf71585b43
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2021-02-03 19:59:37 +0200

    0.0.1

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,23 +1,8 @@
-// MOBILE
-
-const hamburger = document.querySelector(".home_hero_mobile_hamburger_menu");
-const menu = document.querySelector(".home_hero_mobile_menu");
+const hamburger = document.querySelector(".hamburger_menu");
+const menu = document.querySelector(".main_nav_container_right");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("isactive");
   menu.classList.toggle("active");
-  console.log("clicked");
-});
-
-// TABLET
-
-const hamburger_tablet = document.querySelector(
-  ".home_hero_tablet_hamburger_menu"
-);
-const menu_tablet = document.querySelector(".home_hero_tablet_menu");
-
-hamburger_tablet.addEventListener("click", function () {
-  hamburger_tablet.classList.toggle("isactive");
-  menu_tablet.classList.toggle("active");
   console.log("clicked");
 });
